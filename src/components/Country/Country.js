c8168d1f7b36007c961fa4f352eccb6a230ed616
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-30 16:20:05 +0200

    Initialisation &  fetch ->restcountries-API

---
+++ src/components/Country/Country.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "./Country.css";
+
+const Countries = (countries) => {
+  console.log("Countries :>> ", countries);
+
+  return (
+    <div className="country-container">
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={countries.countries.flags.png}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {countries.countries.name.common}
+            <div>Official Name: {countries.countries.name.official}</div>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+          {countries.countries.maps.googleMaps}
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/views/CountriesView/CountriesView.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import Country from "../../components/Country/Country";
+
+const CountriesView = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const result = await response.json();
+    console.log("hallo", result);
+    setCountries(result);
+  };
+
+  return (
+    <div>
+      {countries.map((country) => (
+        <Country countries={country} />
+      ))}
+    </div>
+  );
+};
+
+export default CountriesView;

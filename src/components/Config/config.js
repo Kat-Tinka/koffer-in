74b8610dfcb0b0b0b1981184d97f1df87265536d
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-09-11 17:07:29 +0200

    initialize config & environmental vars inside .env

---
+++ src/components/Config/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqP0J2V1gdNRKyvXiJtz2ZdA2I5scICVE",
+  authDomain: "koffer-in.firebaseapp.com",
+  projectId: "koffer-in",
+  storageBucket: "koffer-in.appspot.com",
+  messagingSenderId: "314709655610",
+  appId: "1:314709655610:web:83fa83af696a40f89ff503",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

918ba620eee3e781c0c73ce8a898ee5492543b3a
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-09-14 19:23:58 +0200

    just changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,87 @@
+// 00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+// import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import Menu from "@mui/material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Container from "@mui/material/Container";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import Tooltip from "@mui/material/Tooltip";
+// import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from "@mui/icons-material/Adb";
+// import Drawer from "@mui/material/Drawer";
+// // import { Link } from "react-router-dom";
+
+// import { useContext, useState } from "react";
+// // import { Button } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { AuthContext } from "../context/authContext";
+
+// ///////////////////////////////////////////////////////////////////////////////
+
+// export default function NavBar() {
+//   const { logout, user } = useContext(AuthContext);
+//   const [open, setisOpen] = useState(false);
+
+//   return (
+//     <AppBar position="sticky">
+//       <Toolbar>
+//         <IconButton
+//           size="large"
+//           edge="start"
+//           color="inherit"
+//           aria-label="menu"
+//           sx={{ mr: 2 }}
+//           onClick={() => setisOpen(!open)}
+//         >
+//           <MenuIcon />
+//         </IconButton>
+//         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//           Koffer in ...{"{ Berlin }"} / <br />
+//           One Little Suitcase in...
+//         </Typography>
+//         <div></div>
+//         <Link to="/">Home </Link>|
+//         {!user ? (
+//           <>
+//             <Button variant="info" href="/register">
+//               {" "}
+//               register
+//             </Button>
+
+//             <Button variant="info" href="/login">
+//               {" "}
+//               login
+//             </Button>
+//           </>
+//         ) : (
+//           <Button variant="info" onClick={logout}>
+//             {" "}
+//             logout
+//           </Button>
+//         )}
+//         <Link to="/chat">Chat </Link>| <Link to="/region">Region </Link>|{" "}
+//         <Drawer open={open} onClose={() => setisOpen(false)} anchor="right">
+//           <Link to="/countries">Countries </Link>
+//         </Drawer>
+//         |{" "}
+//         {/* <Button href="/register" color="inherit">
+//           Register
+//         </Button>
+//         <Button href="/login" color="inherit">
+//           Login
+//         </Button>*/}
+//       </Toolbar>
+//     </AppBar>
+//   );
+// }
+
+//
+
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -12,7 +96,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import Drawer from "@mui/material/Drawer";
+// import Drawer from "@mui/material/Drawer";
 // import { Link } from "react-router-dom";
 
 import { useContext, useState } from "react";
@@ -35,7 +119,7 @@ export default function NavBar() {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
-          onClick={() => setisOpen(!open)}
+          // onClick={() => setisOpen(!open)}
         >
           <MenuIcon />
         </IconButton>
@@ -44,14 +128,15 @@ export default function NavBar() {
           One Little Suitcase in...
         </Typography>
         <div></div>
-        <Link to="/">Home </Link>|
+        <Link to="/">Home </Link>|<Link to="/countries">Countries </Link>|
+        <Link to="/region">Region </Link>|<Link to="/chat">Chat </Link>|
         {!user ? (
           <>
             <Button variant="info" href="/register">
               {" "}
               register
             </Button>
-
+            |
             <Button variant="info" href="/login">
               {" "}
               login
@@ -63,11 +148,9 @@ export default function NavBar() {
             logout
           </Button>
         )}
-        <Link to="/chat">Chat </Link>| <Link to="/region">Region </Link>|{" "}
-        <Drawer open={open} onClose={() => setisOpen(false)} anchor="right">
-          <Link to="/countries">Countries </Link>
-        </Drawer>
-        |{" "}
+        {/* <Drawer open={open} onClose={() => setisOpen(false)} anchor="right"> */}
+        {/* <Link to="/countries">Countries </Link> */}
+        {/* </Drawer> */}|{" "}
         {/* <Button href="/register" color="inherit">
           Register
         </Button> 
